58d95fb38b6c023b89b6fbd2f7691e52456df3f0
John Morgan <johnhmorgan@outlook.com>
2024-11-12 15:19:47 +0000

    Making dashboard page dynamic

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { CirclePlus } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
+import { Invoices } from '@/db/schema';
 
 import {
   Table,
@@ -13,8 +14,12 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import Link from 'next/link';
+import { db } from '@/db';
+import { cn } from '@/lib/utils';
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const invoiceData = await db.select().from(Invoices);
+
   return (
     <main className='flex flex-col justify-center text-center gap-6 max-w-5xl my-12 mx-auto border border-red-500'>
       <div className='flex justify-between'>
@@ -42,25 +47,61 @@ export default function Dashboard() {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <TableRow>
-            <TableCell className='font-medium text-left p-4'>
-              <span className='font-semibold'>31/10/2024</span>
-            </TableCell>
-            <TableCell className='text-left p-4'>
-              <span className='font-semibold'>D Trump</span>
-            </TableCell>
-            <TableCell className='text-left p-4'>
-              <span className=''>dtrump@gmail.com</span>
-            </TableCell>
-            <TableCell className='text-center p-4'>
-              <span className='font-semibold'>
-                <Badge className='rounded-full bg-blue-500'>Open</Badge>
-              </span>
-            </TableCell>
-            <TableCell className='text-right p-4'>
-              <span className='font-semibold'>$250.00</span>
-            </TableCell>
-          </TableRow>
+          {invoiceData.map((invoice) => (
+            <TableRow key={invoice.id}>
+              <TableCell className='font-medium text-left p-0'>
+                <Link
+                  href={`/invoices/${invoice.id}`}
+                  className='font-semibold p-4 block bg-blue-300'
+                >
+                  {new Date(invoice.createTS).toLocaleDateString('en-GB')}
+                </Link>
+              </TableCell>
+              <TableCell className='text-left p-0'>
+                <Link
+                  href={`/invoices/${invoice.id}`}
+                  className='font-semibold p-4 block bg-amber-300'
+                >
+                  D Trump
+                </Link>
+              </TableCell>
+              <TableCell className='text-left p-0'>
+                <Link
+                  href={`/invoices/${invoice.id}`}
+                  className='p-4 block bg-red-300'
+                >
+                  dtrump@gmail.com
+                </Link>
+              </TableCell>
+              <TableCell className='text-center p-0'>
+                <Link
+                  href={`/invoices/${invoice.id}`}
+                  className='font-semibold p-4 block'
+                >
+                  <Badge
+                    className={cn(
+                      'rounded-full',
+                      'capitalize',
+                      invoice.status === 'open' && 'bg-blue-500',
+                      invoice.status === 'paid' && 'bg-green-500',
+                      invoice.status === 'void' && 'bg-zinc-500',
+                      invoice.status === 'uncollectable' && 'bg-red-500',
+                    )}
+                  >
+                    {invoice.status}
+                  </Badge>
+                </Link>
+              </TableCell>
+              <TableCell className='text-right p-0'>
+                <Link
+                  href={`/invoices/${invoice.id}`}
+                  className='font-semibold p-4 block'
+                >
+                  {(invoice.value / 100).toFixed(2)}
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
         </TableBody>
       </Table>
     </main>
