f37e1370d4f0cf179bc1ba8361c69070cadf2c6a
John Morgan <johnhmorgan@outlook.com>
2024-11-13 16:34:28 +0000

    Updating name of app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='flex flex-col h-full justify-center text-center gap-6 max-w-5xl pb-8 mx-auto border border-red-500'>
-      <h1 className='text-5xl font-bold'>Invoice App</h1>
+    <main className='flex flex-col h-full justify-center text-center gap-6 pb-8 mx-auto border border-red-500'>
+      <h1 className='text-5xl font-bold'>Invoice World</h1>
       <p>
         <Button asChild>
           <Link href='/dashboard'>Sign In</Link>
