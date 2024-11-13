e667e6221711e89e1c71481fb8e83b74ed1ae9af
John Morgan <johnhmorgan@outlook.com>
2024-11-13 16:37:29 +0000

    Creating Container component to be used to wrap other components and so standardise style

---
+++ src/components/Container.tsx
@@ -0,0 +1,20 @@
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+interface ContainerProps extends React.ComponentProps<'div'> {}
+
+const Container = ({ children, className, ...props }: ContainerProps) => {
+  return (
+    <div
+      {...props}
+      className={cn(
+        'max-w-5xl mx-auto px-5 border border-green-500',
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Container;
