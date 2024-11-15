435f92a27fbca94c7a008ec2277a7358338a4abf
John Morgan <johnhmorgan@outlook.com>
2024-11-15 10:07:10 +0000

    Adding check for userId inserting userId into new record

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,11 +1,17 @@
 'use server';
 
 import { redirect } from 'next/navigation';
+import { auth } from '@clerk/nextjs/server';
 
 import { Invoices } from '@/db/schema';
 import { db } from '@/db';
 
 export const createAction = async (formData: FormData) => {
+  const { userId, redirectToSignIn } = await auth();
+  if (!userId) {
+    return redirectToSignIn();
+  }
+
   const value = Math.floor(parseFloat(String(formData.get('value'))) * 100);
   const description = formData.get('description') as string;
 
@@ -14,6 +20,7 @@ export const createAction = async (formData: FormData) => {
     .values({
       value,
       description,
+      userId,
       status: 'open',
     })
     .returning({ id: Invoices.id });
