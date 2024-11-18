7b7f37cd5198442a59802aaef707dc05977195d9
John Morgan <johnhmorgan@outlook.com>
2024-11-18 15:46:15 +0000

    Adding typescript coding

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,15 @@
 import { integer, pgTable, pgEnum, timestamp, text } from 'drizzle-orm/pg-core';
 
-export const statusEnum = pgEnum('status', [
-  'open',
-  'paid',
-  'void',
-  'uncollectable',
-]);
+import { AVAILABLE_STATUSES } from '@/data/invoice';
+
+export type Status = (typeof AVAILABLE_STATUSES)[number]['id'];
+
+const statuses = AVAILABLE_STATUSES.map(({ id }) => id) as Array<Status>;
+
+export const statusEnum = pgEnum(
+  'status',
+  statuses as [Status, ...Array<Status>],
+);
 
 export const Invoices = pgTable('invoices', {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
