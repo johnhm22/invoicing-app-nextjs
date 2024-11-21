c9a9c469d5a109a5e0cf4abc46e6c5e5e03f57c2
John Morgan <johnhmorgan@outlook.com>
2024-11-21 15:12:00 +0000

    Adding clerk OrganizationSwitcher to display organisations in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+import {
+  OrganizationSwitcher,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs';
 import Container from '@/components/Container';
 import Link from 'next/link';
 
@@ -8,9 +14,17 @@ const Header = () => {
     <header className='mt-8 my-12'>
       <Container>
         <div className='flex justify-between items-center gap-4'>
-          <p className='font-bold'>
-            <Link href='/dashboard'>Invoice World</Link>
-          </p>
+          <div className='flex flex-row gap-4 items-center'>
+            <p className='font-bold'>
+              <Link href='/dashboard'>invoiceworld</Link>
+            </p>
+            <span className='text-slate-300'>/</span>
+            <SignedIn>
+              <span className='-ml-2'>
+                <OrganizationSwitcher afterCreateOrganizationUrl='/dashboard' />
+              </span>
+            </SignedIn>
+          </div>
 
           <div>
             <SignedOut>
