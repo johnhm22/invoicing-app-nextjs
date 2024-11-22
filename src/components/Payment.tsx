ef333a83dce0d6aa398c90eb81f7e0938a150891
John Morgan <johnhmorgan@outlook.com>
2024-11-22 19:27:19 +0000

    Creating CreditCard component

--- src/components/Payment.tsx
+++ src/components/Payment.tsx
@@ -3,7 +3,7 @@ import { CreditCard } from 'lucide-react';
 
 const Payment = () => {
   return (
-    <div className='flex flex-row gap-3 bg-green-500 w-36 font-bold rounded-sm justify-center text-white py-2 text-sm'>
+    <div className='flex flex-row gap-3 bg-green-700 w-36 font-bold rounded-sm justify-center text-white py-2 text-sm'>
       <CreditCard className='w-5 h-auto' />
       Payment
     </div>
