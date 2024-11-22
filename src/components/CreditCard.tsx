96108eec44dd29521fd3c68b29a97eb5478ea98a
John Morgan <johnhmorgan@outlook.com>
2024-11-22 19:29:54 +0000

    Renaming Payment component to CreditCard

--- src/components/CreditCard.tsx
+++ src/components/CreditCard.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { CreditCard } from 'lucide-react';
 
-const Payment = () => {
+const CreditCardIcon = () => {
   return (
     <div className='flex flex-row gap-3 bg-green-700 w-36 font-bold rounded-sm justify-center text-white py-2 text-sm'>
       <CreditCard className='w-5 h-auto' />
@@ -10,4 +10,4 @@ const Payment = () => {
   );
 };
 
-export default Payment;
+export default CreditCardIcon;
