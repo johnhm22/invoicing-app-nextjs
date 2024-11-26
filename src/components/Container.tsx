250649a5813c81e1120237ccfb68ee1d4282cd0f
John Morgan <johnhmorgan@outlook.com>
2024-11-26 10:47:15 +0000

    Removing unnecessary borders

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -5,10 +5,7 @@ interface ContainerProps extends React.ComponentProps<'div'> {}
 
 const Container = ({ children, className, ...props }: ContainerProps) => {
   return (
-    <div
-      {...props}
-      className={cn('max-w-5xl mx-auto px-5 border border-blue-500', className)}
-    >
+    <div {...props} className={cn('max-w-5xl mx-auto px-5', className)}>
       {children}
     </div>
   );
